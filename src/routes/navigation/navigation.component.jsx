39d6768f5a7598402bb1b3d7376c457d2d3e6c97
Serdar <serdar.onculer.work@hotmail.com>
2022-12-04 01:41:09 +0300

    added addToCart feature

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,11 +6,12 @@ import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from './../../components/cart-dropdown/cart-dropdown.component';
-
 import "./navigation.styles.scss";
 
 
 const Navigation = () => {
+
+
   const { currentUser, setCurrentUser } = useContext(UserContext);
   const {isCartOpen, setIsCartOpen} = useContext(CartContext);
 
