4ca7cb10b8f37e25962b6d7197129159fc17cd06
Serdar <serdar.onculer.work@hotmail.com>
2022-12-04 03:37:38 +0300

    fixed toggle in 'cart-dropdown' component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,28 @@
-import Button from './../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
-import { useContext } from 'react';
-import {CartContext} from "../../contexts/cart.context";
-import {useNavigate} from 'react-router-dom';
-import './cart-dropdown.styles.scss';
+import Button from "./../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import { useNavigate } from "react-router-dom";
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-    const {cartItems} = useContext(CartContext);
-    const navigate = useNavigate();
+  const { cartItems, setIsCartOpen } = useContext(CartContext);
+  const navigate = useNavigate();
 
-    const goToCheckOutHandler = () => {
-        navigate("/checkout");
-    }
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {
-                    cartItems.map((cartItem) => <CartItem key={cartItem.id} cartItem={cartItem} />)
-                }
-            </div>
-            <Button onClick={goToCheckOutHandler}>GO TO CHECKOUT</Button>
-        </div>
-    );
-}
+  const goToCheckOutHandler = () => {
+    navigate("/checkout");
+    setIsCartOpen(false);
+  };
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <Button onClick={goToCheckOutHandler}>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
